eda5e4dc49391e889ce73a3d131fd06277e75993
Ramis Damasceno <ramis.damasceno@youvisa.com.br>
2023-12-13 19:04:49 -0300

    first commit

---
+++ crud.rb
@@ -0,0 +1,44 @@
+movies = {
+  StarWars: 4.8, 
+  Divergent: 4.7
+  }
+
+puts "What would you like to do? "
+
+choice = gets.chomp
+
+case choice
+when "add"
+  puts "What movie would you like to add? "
+  title = gets.chomp
+  if movies[title.to_sym].nil? 
+    puts "What rating does the movie have? "
+    rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+  else
+    puts "That movie already exists! Its rating is #{movies[title.to_sym]}."
+  end
+when "update"
+  puts "What movie would you like to update? "
+  title = gets.chomp
+  if movies[title.to_sym].nil? 
+    puts "That movie does not exist."
+  else
+    puts "What is the new rating? "
+    rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+  end
+when "display"
+  movies.each {|key,value| puts "#{key}: #{value}"} 
+when "delete"
+  puts "What movie do you wanna to delete?"
+  title = gets.chomp
+  if movies[title.to_sym].nil? 
+    puts "Error"
+  else
+    movies.delete(title.to_sym)
+    puts "Movie deleted!"
+  end
+else
+  puts "Error!"
+end
